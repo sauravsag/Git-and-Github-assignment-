b8cdb8985421ffe4b13df4f28c8b2c7fe2fa322f
sauravsag <146501978+sauravsag@users.noreply.github.com>
2024-07-25 18:54:28 +0530

    Git and Github assignmet solution

---
+++ Git and Github/project/script.js
@@ -0,0 +1,13 @@
+function sayHello(){
+     console.log("hello world! this change is made in main branch.");
+}
+
+function personalizedGreeting(name){
+	console.log(`Hello, ${name}! have a greet day`);
+}
+
+function updateFeature(){
+	console.log("feature update in progress .....")
+} 
+
+
